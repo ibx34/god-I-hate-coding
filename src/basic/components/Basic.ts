3fe100d1c22084ef1cd1380cf554b6ebf9e243d3
ibx34 <51100404+ibx34@users.noreply.github.com>
2020-11-15 14:41:14 -0800

    Boom

---
+++ src/basic/components/Basic.ts
@@ -0,0 +1,28 @@
+import { Component, ComponentAPI, Inject, Subscribe } from '@ayanaware/bento';
+import { Discord, DiscordEvent } from '@ayanaware/bentocord';
+
+import { Logger } from '@ayanaware/logger';
+import { Message } from 'eris';
+const log = Logger.get();
+
+export class Basic implements Component {
+    public name = 'Basic';
+    public api!: ComponentAPI;
+
+    @Inject(Discord) private discord: Discord;
+
+    @Subscribe(Discord, DiscordEvent.SHARD_READY)
+    private onShardReady(id: number) {
+        log.info(`Shard "${id}" ready`);
+    }
+
+    @Subscribe(Discord, DiscordEvent.SHARD_DISCONNECT)
+    private onShardDisconnect(e: Error, id: number) {
+        log.info(`Shard "${id}" disconnect`);
+    }
+
+    @Subscribe(Discord, DiscordEvent.MESSAGE_CREATE)
+    private async onMessageCreate(message: Message) {
+        log.info(`${message.author.username}#${message.author.discriminator}: ${message.content}`);
+    }
+}
---
+++ src/basic/index.ts
@@ -0,0 +1,26 @@
+import { Bento, FSComponentLoader, VariableFileLoader } from '@ayanaware/bento';
+import { Bentocord, BentocordVariable } from '@ayanaware/bentocord';
+
+
+const bento = new Bento();
+
+(async () => {
+    const vfl = new VariableFileLoader();
+    await vfl.addFile([__dirname, '..', 'config.json']);
+    await bento.addPlugin(vfl);
+
+    const bentocord = new Bentocord();
+
+    const fsloader = new FSComponentLoader();
+    await fsloader.addDirectory(__dirname, 'components');
+
+    await bento.addPlugin(vfl);
+    if (!bento.hasVariable(BentocordVariable.BENTOCORD_TOKEN)) throw new Error(`Please append ${BentocordVariable.BENTOCORD_TOKEN}=xxx to the front of your command`);
+
+    await bento.addPlugins([bentocord, fsloader]);
+
+    await bento.verify();
+})().catch(e => {
+    console.log(e);
+    process.exit(1);
+});
